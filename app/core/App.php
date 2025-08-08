ae08c97266d743d14cb63baf8e4477587752e113
iruzz <rabbaniruzz45@gmail.com>
2025-08-08 21:03:55 +0700

    routing

--- app/core/App.php
+++ app/core/App.php
@@ -3,6 +3,14 @@
 class App {
     public function __construct() {
         // Initialization code for the application
-        echo "Application initialized.";
+        $url = $this->parseURL();
+        var_dump($url);
+    }
+
+    public function parseURL() {
+        if (isset($_GET['url'])){
+            $url = $_GET['url'];
+            return $url;
+        }
     }
 }
